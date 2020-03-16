1fc1d4e8222bee4f6a3a022f14e4979518d64739
Jean Pierre Velez <vcjpierre@gmail.com>
2020-03-16 02:45:22 -0400

    Create checkout form

---
+++ resources/views/cart/checkout.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+@section('content')
+
+<h2>Checkout</h2>
+<h3>Shipping Information</h3>
+<form action="{{route('orders.store')}}" method="post">
+    @csrf
+
+    <div class="form-group">
+        <label for="">Full Name</label>
+        <input type="text" name="shipping_fullname" id="" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="">State</label>
+        <input type="text" name="shipping_state" id="" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="">City</label>
+        <input type="text" name="shipping_city" id="" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="">Zip</label>
+        <input type="number" name="shipping_zipcode" id="" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="">Full Address</label>
+        <input type="text" name="shipping_address" id="" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="">Mobile</label>
+        <input type="text" name="shipping_phone" id="" class="form-control">
+    </div>
+
+    <h4>Payment option</h4>
+
+    <div class="form-check">
+        <label class="form-check-label">
+            <input type="radio" class="form-check-input" name="payment_method" id="" value="cash_on_delivery">
+            Cash on delivery
+        </label>
+    </div>
+
+    <div class="form-check">
+        <label class="form-check-label">
+            <input type="radio" class="form-check-input" name="payment_method" id="" value="paypal">
+            Paypal
+        </label>
+    </div>
+
+    <button type="submit" class="btn btn-primary mt-3">Place Order</button>
+</form>
+@endsection
