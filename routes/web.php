0b73f0fbe025c3e8a859e77f89ac721354fee7d0
Jean Pierre Velez <vcjpierre@gmail.com>
2020-03-16 02:46:15 -0400

    Add orders and checkout routes

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,7 @@ Route::get('/add-to-cart/{product}', 'CartController@add')->name('cart.add')->mi
 Route::get('/cart', 'CartController@index')->name('cart.index')->middleware('auth');
 Route::get('/cart/destroy/{itemId}', 'CartController@destroy')->name('cart.destroy')->middleware('auth');
 Route::get('/cart/update/{itemId}', 'CartController@update')->name('cart.update')->middleware('auth');
+Route::get('/cart/checkout', 'CartController@checkout')->name('cart.checkout')->middleware('auth');
+
+//Orders 
+Route::resource('orders', 'OrderController')->middleware('auth');
