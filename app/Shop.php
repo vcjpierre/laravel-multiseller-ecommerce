a9dc7fa19bb76dd591d494c99da7b3712c0da823
Jean Pierre Velez <vcjpierre@gmail.com>
2020-03-21 21:36:55 -0400

    Add function owner

--- app/Shop.php
+++ app/Shop.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shop extends Model
 {
-    //
+    protected $fillable=['name','description'];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
