8739cabf243210af3a16ad40d5c2f571d895ba03
Jean Pierre Velez <vcjpierre@gmail.com>
2020-03-21 21:34:26 -0400

    Update shop table

--- database/migrations/2020_03_22_000227_create_shops_table.php
+++ database/migrations/2020_03_22_000227_create_shops_table.php
@@ -14,7 +14,15 @@ class CreateShopsTable extends Migration
     public function up()
     {
         Schema::create('shops', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->boolean('is_active')->default(false);
+
+            $table->text('description')->nullable();
+            $table->float('rating')->nullable();
             $table->timestamps();
         });
     }
