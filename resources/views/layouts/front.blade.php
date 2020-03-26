a7b4e50e272d9ac272084b406c87ae556bb0d1b6
Jean Pierre Velez <vcjpierre@gmail.com>
2020-03-26 00:39:01 -0400

    Search products

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -123,9 +123,9 @@
                         </div>
                     </div>
                     <div class="categories-wrapper">
-                        <form action="#">
-                            <input placeholder="Enter Your key word" type="text">
-                            <button type="button"> Search </button>
+                        <form action="{{route('products.search')}}" method="GET">
+                            <input name="query" placeholder="Enter your key word" type="text">
+                            <button type="submit"> Search </button>
                         </form>
                     </div>
                 </div>
@@ -140,60 +140,7 @@
                             @endauth
                                 Item
                         </span>
-                    </a>
-                    {{-- <ul class="cart-dropdown">
-                        <li class="single-product-cart">
-                            <div class="cart-img">
-                                <a href="#"><img src="{{asset('img/cart/1.jpg')}}" alt=""></a>
-                            </div>
-                            <div class="cart-title">
-                                <h5><a href="#"> Bits Headphone</a></h5>
-                                <h6><a href="#">Black</a></h6>
-                                <span>$80.00 x 1</span>
-                            </div>
-                            <div class="cart-delete">
-                                <a href="#"><i class="ti-trash"></i></a>
-                            </div>
-                        </li>
-                        <li class="single-product-cart">
-                            <div class="cart-img">
-                                <a href="#"><img src="{{asset('img/cart/2.jpg')}}" alt=""></a>
-                            </div>
-                            <div class="cart-title">
-                                <h5><a href="#"> Bits Headphone</a></h5>
-                                <h6><a href="#">Black</a></h6>
-                                <span>$80.00 x 1</span>
-                            </div>
-                            <div class="cart-delete">
-                                <a href="#"><i class="ti-trash"></i></a>
-                            </div>
-                        </li>
-                        <li class="single-product-cart">
-                            <div class="cart-img">
-                                <a href="#"><img src="{{asset('img/cart/3.jpg')}}" alt=""></a>
-                            </div>
-                            <div class="cart-title">
-                                <h5><a href="#"> Bits Headphone</a></h5>
-                                <h6><a href="#">Black</a></h6>
-                                <span>$80.00 x 1</span>
-                            </div>
-                            <div class="cart-delete">
-                                <a href="#"><i class="ti-trash"></i></a>
-                            </div>
-                        </li>
-                        <li class="cart-space">
-                            <div class="cart-sub">
-                                <h4>Subtotal</h4>
-                            </div>
-                            <div class="cart-price">
-                                <h4>$240.00</h4>
-                            </div>
-                        </li>
-                        <li class="cart-btn-wrapper">
-                            <a class="cart-btn btn-hover" href="#">view cart</a>
-                            <a class="cart-btn btn-hover" href="#">checkout</a>
-                        </li>
-                    </ul> --}}
+                    </a>                   
                 </div>
                 <div class="mobile-menu-area mobile-menu-none-block electro-2-menu">
                     <div class="mobile-menu">
