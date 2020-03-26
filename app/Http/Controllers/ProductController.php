d08768c8877e482a357ffccca57d4ff263eb3408
Jean Pierre Velez <vcjpierre@gmail.com>
2020-03-26 00:38:49 -0400

    Add search function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,11 +8,6 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $categoryId = request('category_id');
@@ -21,8 +16,8 @@ class ProductController extends Controller
         if ($categoryId) {
             $category = Category::find($categoryId);
             $categoryName = ucfirst($category->name);
-            $products = $category->allProducts();
 
+            $products = $category->allProducts();
         } else {
             $products = Product::take(10)->get();
         }
@@ -30,69 +25,11 @@ class ProductController extends Controller
         return view('product.index', compact('products', 'categoryName'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function search(Request $request)
     {
-        //
-    }
+        $query = $request->input('query');
+        $products = Product::where('name','LIKE',"%$query%")->paginate(10);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Product $product)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product $product)
-    {
-        //
+        return view('product.catalog',compact('products'));
     }
 }
