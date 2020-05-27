359af941964e0099cf9b121b14cdbc6978c27081
Jean Pierre Velez <vcjpierre@gmail.com>
2020-05-26 23:01:29 -0400

    Create update cart form component

---
+++ resources/views/livewire/cart-update-form.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <form wire:submit.prevent='updateCart'>
+        <input wire:model="quantity" type="number">
+        <input class="button" type="submit" value="save">
+    </form>
+</div>
