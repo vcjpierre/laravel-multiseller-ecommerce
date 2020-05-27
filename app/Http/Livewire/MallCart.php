af2b9cebdc4e39d73f48153def9d8b115d71fd1b
Jean Pierre Velez <vcjpierre@gmail.com>
2020-05-26 23:01:12 -0400

    Create mall cart component

---
+++ app/Http/Livewire/MallCart.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class MallCart extends Component
+{
+    public $cartItems = [];
+
+    protected $listeners = ['cartUpdated' => 'onCartUpdate'];
+
+    public function mount()
+    {
+        $this->cartItems = \Cart::session(auth()->id())->getContent()->toArray();
+    }
+    
+    public function onCartUpdate()
+    {
+        $this->mount();
+    }
+
+    public function render()
+    {
+        return view('livewire.mall-cart');
+    }
+}
