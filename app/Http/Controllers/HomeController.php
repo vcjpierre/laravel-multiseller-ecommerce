92488aff6778dddd683e3ad4db31f735c443cb16
Jean Pierre Velez <vcjpierre@gmail.com>
2021-08-31 15:18:46 -0400

    Refactor and optimization

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Product;
-use TCG\Voyager\Models\Category;
 use Illuminate\Http\Request;
+use TCG\Voyager\Models\Category;
 
 class HomeController extends Controller
 {
@@ -25,9 +25,17 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $products = Product::take(30)->get();
+        $products = Product::with('shop.owner')->take(30)->get();
+
         $categories = Category::whereNull('parent_id')->get();
 
+        //$categories = Category::with('children.children')->whereNull('parent_id')->get();
+
         return view('home', ['allProducts' => $products,'categories'=>$categories]);
+    }
+
+    public function contact()
+    {
+        return view('contact');
     }
 }
---
+++ resources/views/_slider.blade.php
@@ -0,0 +1,11 @@
+<nav>
+    <ul>
+        <li>
+            <a href="{{url('/')}}">home </a>
+        </li>
+        <li>
+            <a href="#">blog <span class="sticker-new">hot</span></a>
+        </li>
+        <li><a href="#">contact</a></li>
+    </ul>
+</nav>
